320eff53dfa2cde8032a5e066499f4da0b5064a2
chenyuying <cyy1229@163.com>
2021-11-06 18:55:27 +0800

    随便写写

--- QUANTAXIS/example/TushareDataLoader.py
+++ QUANTAXIS/example/TushareDataLoader.py
@@ -2,11 +2,19 @@ from QUANTAXIS import QA_Setting
 
 QASETTING = QA_Setting()
 DATABASE = QASETTING.client.quantaxis
+from loguru import logger
+import pandas as pd
+
 
 class TushareDataLoader():
     def __init__(self):
-        pass
+        self.stock_day_df = pd.DataFrame([item for item in DATABASE.tushare_stock_day.find({})])
+        logger.info("get tushare_stock_day all count = {}", self.stock_day_df.shape[0])
 
     def query_all_stock_day(self):
         # DATABASE.
         pass
+
+
+if __name__ == '__main__':
+    TushareDataLoader()
